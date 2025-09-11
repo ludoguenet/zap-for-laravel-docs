18b2f6454e431df53e8334692d530923dead8f6a
ludoguenet <ludovicguenet@gmx.com>
2025-09-11 09:35:09 +0200

    fix: new colours; chore: bump dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,14 @@
 export default defineNuxtConfig({
-  extends: ['docus'],
-   content: {
-    build: {
-      markdown: {
-        highlight: {
-            langs: ['json', 'js', 'ts', 'html', 'css', 'vue', 'shell', 'mdc', 'md', 'yaml', 'php']
+    extends: ['docus'],
+    modules: ['@nuxt/ui-pro'],
+    css: ['~/assets/css/main.css'],
+    content: {
+        build: {
+            markdown: {
+            highlight: {
+                    langs: ['json', 'js', 'ts', 'html', 'css', 'vue', 'shell', 'mdc', 'md', 'yaml', 'php']
+                }
+            }
         }
-      }
     }
-  }
 })
